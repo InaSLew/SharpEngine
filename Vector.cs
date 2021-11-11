3257fe7c4398e9b3f82e23deef013d20975f5dd5
InaSLew <ina.shenlewander@gmail.com>
2021-11-11 18:14:59 +0100

    Put Color struct into separate file

--- Vector.cs
+++ Vector.cs
@@ -37,19 +37,4 @@ namespace SharpEngine
         public static Vector Max(Vector a, Vector b) => new (MathF.Max(a.x, b.x),MathF.Max(a.y, b.y));
         public static Vector Min(Vector a, Vector b) => new (MathF.Min(a.x, b.x),MathF.Min(a.y, b.y));
     }
-    public struct Color
-    {
-        public static readonly Color Red = new Color(1, 0, 0, 1);
-        public static readonly Color Green = new Color(0, 1, 0, 1);
-        public static readonly Color Blue = new Color(0, 0, 1, 1);
-        public float R, G, B, A;
-
-        public Color(float r, float g, float b, float a)
-        {
-            R = r;
-            G = g;
-            B = b;
-            A = a;
-        }
-    }
 }
