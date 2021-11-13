d3f86d5085a45a6423c0dd69d0154571bd2df8a2
InaSLew <ina.shenlewander@gmail.com>
2021-11-13 22:26:53 +0100

    Get started with Cone class

---
+++ Cone.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpEngine
+{
+    public class Cone : Shape
+    {
+        // public Cone(float radius, float angle, Vector position)
+        private float radius, angle;
+        private Vector position;
+
+        public Cone(Vertex[] vertices, float radius, float angle, Vector position) : base(vertices)
+        {
+            this.radius = radius;
+            this.angle = angle;
+            this.position = position;
+        }
+    }
+}
