83ef3234b253e2f728f33df684ea1723b4e55551
InaSLew <ina.shenlewander@gmail.com>
2021-11-15 09:34:45 +0100

    Failed cone attempt

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ namespace SharpEngine
 {
     class Program
     {
-        static Shape triangle = new Triangle(.2f, .2f, new Vector(0, 0));
-        static Shape triangle2 = new Triangle(.3f, .4f, new Vector(-.3f, 0));
-        static Shape rectangle = new Rectangle(.3f, .4f, new Vector(.3f, 0));
-        private static Shape circle = new Circle(.3f, new Vector(-.2f, 0));
+        // static Shape triangle = new Triangle(.2f, .2f, new Vector(0, 0));
+        // static Shape triangle2 = new Triangle(.3f, .4f, new Vector(-.3f, 0));
+        // static Shape rectangle = new Rectangle(.3f, .4f, new Vector(.3f, 0));
+        // private static Shape circle = new Circle(.3f, new Vector(-.2f, 0));
+        private static Shape cone = new Cone(.3f, 45f, new Vector(0, 0));
 
         private const int Width = 1024;
         private const int Height = 768;
@@ -22,10 +23,10 @@ namespace SharpEngine
             var multiplier = 0.999f;
             var multiplier2 = 0.985f;
             
-            triangle.SetMoveDirection((Vector.Right + Vector.Down) * 3);
-            triangle2.SetMoveDirection((Vector.Left + Vector.Down) * 3);
-            rectangle.SetMoveDirection((Vector.Right + Vector.Up) * 3);
-            circle.SetMoveDirection((Vector.Left + Vector.Up) * 3);
+            // triangle.SetMoveDirection((Vector.Right + Vector.Down) * 3);
+            // triangle2.SetMoveDirection((Vector.Left + Vector.Down) * 3);
+            // rectangle.SetMoveDirection((Vector.Right + Vector.Up) * 3);
+            // circle.SetMoveDirection((Vector.Left + Vector.Up) * 3);
             
             while (!Glfw.WindowShouldClose(window))
             {
@@ -33,53 +34,53 @@ namespace SharpEngine
                 ClearScreen();
                 Render();
                 
-                triangle.Scale(multiplier);
-                if (triangle.CurrentScale <= 0.5f)
-                {
-                    multiplier = 1.0001f;
-                }
-                if (triangle.CurrentScale >= 1f)
-                {
-                    multiplier = 0.9999f;
-                }
-                triangle.Move();
-                triangle.Rotate(.1f);
-                
-                triangle2.Scale(multiplier2);
-                if (triangle2.CurrentScale <= 0.5f)
-                {
-                    multiplier2 = 1.001f;
-                }
-                if (triangle2.CurrentScale >= 1f)
-                {
-                    multiplier2 = 0.999f;
-                }
-                triangle2.Move();
-                triangle2.Rotate(-.08f);
-                
-                rectangle.Scale(multiplier);
-                if (rectangle.CurrentScale <= 0.5f)
-                {
-                    multiplier = 1.0001f;
-                }
-                if (rectangle.CurrentScale >= 1f)
-                {
-                    multiplier = 0.9999f;
-                }
-                rectangle.Move();
-                rectangle.Rotate(-.05f);
-                
-                circle.Scale(multiplier2);
-                if (circle.CurrentScale <= 0.5f)
-                {
-                    multiplier = 1.0001f;
-                }
-                if (circle.CurrentScale >= 1f)
-                {
-                    multiplier = 0.9999f;
-                }
-                circle.Move();
-                circle.Rotate(-.05f);
+                // triangle.Scale(multiplier);
+                // if (triangle.CurrentScale <= 0.5f)
+                // {
+                //     multiplier = 1.0001f;
+                // }
+                // if (triangle.CurrentScale >= 1f)
+                // {
+                //     multiplier = 0.9999f;
+                // }
+                // triangle.Move();
+                // triangle.Rotate(.1f);
+                //
+                // triangle2.Scale(multiplier2);
+                // if (triangle2.CurrentScale <= 0.5f)
+                // {
+                //     multiplier2 = 1.001f;
+                // }
+                // if (triangle2.CurrentScale >= 1f)
+                // {
+                //     multiplier2 = 0.999f;
+                // }
+                // triangle2.Move();
+                // triangle2.Rotate(-.08f);
+                //
+                // rectangle.Scale(multiplier);
+                // if (rectangle.CurrentScale <= 0.5f)
+                // {
+                //     multiplier = 1.0001f;
+                // }
+                // if (rectangle.CurrentScale >= 1f)
+                // {
+                //     multiplier = 0.9999f;
+                // }
+                // rectangle.Move();
+                // rectangle.Rotate(-.05f);
+                //
+                // circle.Scale(multiplier2);
+                // if (circle.CurrentScale <= 0.5f)
+                // {
+                //     multiplier = 1.0001f;
+                // }
+                // if (circle.CurrentScale >= 1f)
+                // {
+                //     multiplier = 0.9999f;
+                // }
+                // circle.Move();
+                // circle.Rotate(-.05f);
             }
 
             Glfw.Terminate();
@@ -93,10 +94,11 @@ namespace SharpEngine
 
         private static void Render()
         {
-            triangle.Render();
-            triangle2.Render();
-            rectangle.Render();
-            circle.Render();
+            // triangle.Render();
+            // triangle2.Render();
+            // rectangle.Render();
+            // circle.Render();
+            cone.Render();
             glFlush();
         }
 
