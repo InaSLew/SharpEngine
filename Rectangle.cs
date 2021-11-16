c14e3a89e8784b7f7528007e95cb2b26e054d12f
InaSLew <ina.shenlewander@gmail.com>
2021-11-16 12:01:32 +0100

    Introduced Rectangle.cs back in

--- Rectangle.cs
+++ Rectangle.cs
@@ -2,36 +2,36 @@
 
 namespace SharpEngine
 {
-    // public class Rectangle : Shape
-    // {
-    //     public float width, height;
-    //     public Vector position;
-    //
-    //     public Rectangle(float width, float height, Vector position) : base(new Vertex[4])
-    //     {
-    //         this.width = width;
-    //         this.height = height;
-    //         this.position = position;
-    //         vertices = GetVertices();
-    //     }
-    //
-    //     private Vertex[] GetVertices()
-    //     {
-    //         var result = new Vertex[6];
-    //         result[0] = new Vertex(position, Color.Red);
-    //         result[1] = new Vertex(new Vector(position.x + width, position.y), Color.Green);
-    //         result[2] = new Vertex(new Vector(position.x, position.y + height), Color.Blue);
-    //         result[3] = new Vertex(new Vector(position.x + width, position.y + height), Color.Red);
-    //         result[4] = new Vertex(new Vector(position.x + width, position.y), Color.Green);
-    //         result[5] = new Vertex(new Vector(position.x, position.y + height), Color.Blue);
-    //         
-    //         return result;
-    //     }
-    //     
-    //     public override void Render()
-    //     {
-    //         base.Render();
-    //         glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
-    //     }
-    // }
+    public class Rectangle : Shape
+    {
+        public float width, height;
+        public Vector position;
+    
+        public Rectangle(float width, float height, Vector position, Material material) : base(new Vertex[4], material)
+        {
+            this.width = width;
+            this.height = height;
+            this.position = position;
+            vertices = GetVertices();
+        }
+    
+        private Vertex[] GetVertices()
+        {
+            var result = new Vertex[6];
+            result[0] = new Vertex(position, Color.Red);
+            result[1] = new Vertex(new Vector(position.x + width, position.y), Color.Green);
+            result[2] = new Vertex(new Vector(position.x, position.y + height), Color.Blue);
+            result[3] = new Vertex(new Vector(position.x + width, position.y + height), Color.Red);
+            result[4] = new Vertex(new Vector(position.x + width, position.y), Color.Green);
+            result[5] = new Vertex(new Vector(position.x, position.y + height), Color.Blue);
+            
+            return result;
+        }
+        
+        public override void Render()
+        {
+            base.Render();
+            glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
+        }
+    }
 }
